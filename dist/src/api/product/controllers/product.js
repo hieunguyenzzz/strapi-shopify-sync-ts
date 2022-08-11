429f02984ef681632c845c1b17c587b998f356a1
Hieu Nguyen <hieunguyenel@gmail.com>
2022-08-11 14:28:43 +0700

    add graphql

--- dist/src/api/product/controllers/product.js
+++ dist/src/api/product/controllers/product.js
@@ -5,6 +5,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const strapi_1 = require("@strapi/strapi");
 exports.default = strapi_1.factories.createCoreController('api::product.product', ({ strapi }) => ({
+    // Method 1: Creating an entirely custom action
+    async exampleAction(ctx) {
+        try {
+            ctx.body = 'ok';
+        }
+        catch (err) {
+            ctx.body = err;
+        }
+    },
     async findOne(ctx) {
         // some logic here
         const response = await super.findOne(ctx);
--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -5,6 +5,15 @@
 import { factories } from '@strapi/strapi'
 
 export default factories.createCoreController('api::product.product', ({strapi}) => ({
+  // Method 1: Creating an entirely custom action
+  async exampleAction(ctx) {
+    try {
+      ctx.body = 'ok';
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+
   async findOne(ctx) {
     // some logic here
     const response = await super.findOne(ctx);
